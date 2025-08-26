0bc049370a129b951586f5c934078d04d9190a5d
Aina Ayomide <joytife8@gmail.com>
2025-08-26 22:50:26 +0100

    feat:improved blog styling

--- workshop/src/pages/Blog.jsx
+++ workshop/src/pages/Blog.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Link } from "react-router-dom";
 import Nav from '../components/Nav';
+import Footer from '../components/Footer';
 
 const Blog= () => {
     return(
@@ -22,16 +23,16 @@ const Blog= () => {
               <div className="space-y-8">
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-6xl mx-auto">
                   <div className="">
-                    <img className="product-image my-5 md:mb-5 w-full h-50 sm:h-56 md:h-64 lg:h-80 object-cover rounded-lg shadow-sm"
+                    <img className="product-image max-w-sm mx-auto my-5 md:mb-5 w-full h-48 sm:h-56 md:h-64 lg:h-80 object-cover rounded-lg shadow-sm"
                       src="https://tz47gb.yourbrand.studio/files/dynamicContent/sites/tz47gb/images/en/newspage_17/m66gvpat/element_328/rwdMode_1/930x500/Workshop_header_blog_A.webp" 
                       alt="" 
                     />
                   </div>
 
-                  <div>
-                    <h2 className= "text-lg mt-7 font-bold">What affects the quality of Ceramic products ?</h2>
-                    <p className= "text-gray-500 my-3 ">17 August 2022</p>
-                    <p className= "text-gray-800 mb-3 ">
+                  <div className="px-6 py-3">
+                    <h2 className= "text-lg mt-7 font-bold mb-2">What affects the quality of Ceramic products ?</h2>
+                    <p className= "text-gray-500 mb-3  text-sm">17 August 2022</p>
+                    <p className= "text-gray-800 mb-4 text-sm">
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum nisi nec massa rhoncus venenatis. 
                       Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus suscipit...
                     </p>
@@ -47,14 +48,14 @@ const Blog= () => {
 
 
                   <div className="">
-                    <img className="product-image my-5 md:mb-5 w-full h-50 sm:h-56 md:h-64 lg:h-80 object-cover rounded-lg shadow-sm"
+                    <img className="product-image max-w-sm mx-auto my-5 md:mb-5 w-full h-48 sm:h-56 md:h-48 lg:h-80 object-cover rounded-lg shadow-sm"
                       src="https://tz47gb.yourbrand.studio/files/dynamicContent/sites/tz47gb/images/en/newspage_16/m66gvqb6/element_328/rwdMode_1/930x500/Workshop_3_blog_B.webp" 
                       alt="" 
                     />
                   </div>
 
-                  <div>
-                    <h2 className= "text-lg mt-7 font-bold">How Ceramics are made ?</h2>
+                  <div className="px-6 py-3">
+                    <h2 className= "text-lg mt-7 font-bold ">How Ceramics are made ?</h2>
                     <p className= "text-gray-500 my-3">17 August 2022</p>
                     <p className= "text-gray-800 mb-3 ">
                       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum nisi nec massa rhoncus venenatis. 
@@ -134,9 +135,75 @@ const Blog= () => {
                 </div>
             </div>
 
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 max-w-6xl mx-auto mt-15">
+              <div>
+                <img className="product-image max-w-sm mx-auto my-5 md:mb-5 w-full h-48 sm:h-56 md:h-64 lg:h-80 object-cover rounded-lg shadow-sm"
+                  src="https://tz47gb.yourbrand.studio/files/dynamicContent/sites/tz47gb/images/en/newspage_18/m66gvobu/element_328/rwdMode_1/930x500/blog_3.webp" 
+                  alt=""
+                />
+                <div className="px-6 py-3">
+                  <h2 className= "text-lg mt-7 font-bold mb-2">Ceramics soup bowl</h2>
+                  <p className= "text-gray-500 m-3  text-sm">17 August 2022</p>
+                  <p className= "text-gray-800 m-2 text-sm lg:text-lg">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum nisi nec massa rhoncus venenatis. 
+                    Vestibulum ante ipsum primis in...
+                  </p>
+                  <a 
+                    href="https://tz47gb.yourbrand.studio/what-affects-the-quality-of-ceramic-products"
+                    className="px-3 text-gray-500 cursor-pointer font-medium"
+                    target="_blank">
+                    Read more
+                  </a>
+                </div>
+              </div>
 
+              {/*  */}
+              <div>
+                <img className="product-image max-w-sm mx-auto my-5 md:mb-5 w-full h-48 sm:h-56 md:h-64 lg:h-80 object-cover rounded-lg shadow-sm"
+                  src="https://tz47gb.yourbrand.studio/files/dynamicContent/sites/tz47gb/images/en/newspage_17/m66gvpat/element_328/rwdMode_1/930x500/Workshop_header_blog_A.webp" 
+                  alt=""
+                />
+                <div className="px-6 py-3">
+                  <h2 className= "text-lg mt-7 font-bold mb-2">What affects the quality of Ceramics products</h2>
+                  <p className= "text-gray-500 mb-3  text-sm">17 August 2022</p>
+                  <p className= "text-gray-800 mb-2 text-smc lg:text-lg">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum nisi nec massa rhoncus venenatis. 
+                    Vestibulum ante ipsum primis in...
+                  </p>
+                  <a 
+                    href="https://tz47gb.yourbrand.studio/what-affects-the-quality-of-ceramic-products"
+                    className="text-gray-500 cursor-pointer font-medium"
+                    target="_blank">
+                    Read more
+                  </a>
+                </div>
+              </div>
 
+              {/*  */}
+              <div>
+                <img className="product-image max-w-sm mx-auto my-5 md:mb-5 w-full h-48 sm:h-56 md:h-64 lg:h-80 object-cover rounded-lg shadow-sm"
+                  src="https://tz47gb.yourbrand.studio/files/dynamicContent/sites/tz47gb/images/en/newspage_16/m66gvqb6/element_328/rwdMode_1/930x500/Workshop_3_blog_B.webp" 
+                  alt=""
+                />
+                <div className="px-6 py-3">
+                  <h2 className= "text-lg mt-7 font-bold mb-2">How Ceramics are made</h2>
+                  <p className= "text-gray-500 mb-3  text-sm">17 August 2022</p>
+                  <p className= "text-gray-800 mb-2 text-sm lg:text-lg">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam fermentum nisi nec massa rhoncus venenatis. 
+                    Vestibulum ante ipsum primis in...
+                  </p>
+                  <a 
+                    href="https://tz47gb.yourbrand.studio/what-affects-the-quality-of-ceramic-products"
+                    className="text-gray-500 cursor-pointer font-medium"
+                    target="_blank">
+                    Read more
+                  </a>
+                </div>
+              </div>
+            </div>
+            <hr className="my-10 max-w-6xl mx-auto text-gray-700"/>
           </section>
+          <Footer/>
         </>
     );
 }
